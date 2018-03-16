151dae7649869a8dc7d03e42edb2875ef454a35c
Joe Reeve <joseph.reeve@googlemail.com>
2018-03-16 11:20:57 +0000

    Added logging to shell scripts

--- consume-mps-at.sh
+++ consume-mps-at.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-java -cp target/pepper-box-1.0.jar:.  com.gslab.pepper.PepperBoxLoadConsumer --consumer-config-file pblg.properties --num-consumers 3  --topic-name mps.$1 --per-thread-topics YES --test-duration 20 --throughput-per-consumer $1 --starting-offset 0 &> consume_mps_at.$1.log
+echo "Running pepper-box consumer on [$(hostname)]"
+echo "Args: [mps=$1] [test-duration=$2]"
+java -cp target/pepper-box-1.0.jar:.  com.gslab.pepper.PepperBoxLoadConsumer --consumer-config-file pblg.properties --num-consumers 3  --topic-name mps.$1 --per-thread-topics YES --test-duration $2 --throughput-per-consumer $1 --starting-offset 0 &> consume_mps_at.$1.log
--- produce-mps-at.sh
+++ produce-mps-at.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-java -cp target/pepper-box-1.0.jar:.  com.gslab.pepper.PepperBoxLoadGenerator --schema-file schema1000.txt --producer-config-file pblg.properties --topic-name mps.$1 --per-thread-topics YES  --throughput-per-producer $1 --test-duration 20 --num-producers 3  --starting-offset 0 &> produce_mps_at.$1.log
+echo "Running pepper-box consumer on [$(hostname)]"
+echo "Args: [mps=$1] [test-duration=$2]"
+java -cp target/pepper-box-1.0.jar:.  com.gslab.pepper.PepperBoxLoadGenerator --schema-file schema1000.txt --producer-config-file pblg.properties --topic-name mps.$1 --per-thread-topics YES  --throughput-per-producer $1 --test-duration $2 --num-producers 3  --starting-offset 0 &> produce_mps_at.$1.log
 
